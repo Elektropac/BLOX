9e2db99b8e733dfe3ef15c77d9bbd062ae995512
ElektroPac <43667224+Elektropac@users.noreply.github.com>
2025-04-26 17:06:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False, ssl_context=('/opt/blox-webui/certs/cert.pem', '/opt/blox-webui/certs/key.pem'))
