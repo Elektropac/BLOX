acb5189e855e8a83cb6f717cf315cbf89eb8b9eb
ElektroPac <43667224+Elektropac@users.noreply.github.com>
2025-04-26 20:21:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import threading
 
 app = Flask(__name__)
 
@@ -6,5 +7,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+def run_http():
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
+def run_https():
+    app.run(host='0.0.0.0', port=5001, debug=False, ssl_context=('/opt/blox-webui/certs/cert.pem', '/opt/blox-webui/certs/key.pem'))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False, ssl_context=('/opt/blox-webui/certs/cert.pem', '/opt/blox-webui/certs/key.pem'))
+    threading.Thread(target=run_http).start()
+    threading.Thread(target=run_https).start()
